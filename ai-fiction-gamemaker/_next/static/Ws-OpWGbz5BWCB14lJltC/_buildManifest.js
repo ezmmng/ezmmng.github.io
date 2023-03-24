52137fb8d9f78bf6a48bfe59e876f380f8a95b3e
ezm2ng <ezm2ng@github.io>
2023-03-24 12:55:10 +0000

    Deploy by GitHub Actions (ai-fiction-gamemaker)

--- ai-fiction-gamemaker/_next/static/Ws-OpWGbz5BWCB14lJltC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-14298a6081bcedb2.js","static/css/043e7b72487eed74.css","static/chunks/pages/index-749f321a6f236a4d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
